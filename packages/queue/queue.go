66dc5d16e319f3e257d46aa6b0091381f8307c84
Divya Muppaneni <divya.muppaneni@safeguardproperties.com>
2017-01-26 09:04:19 -0500

    Already have the interface to save in database

--- packages/queue/queue.go
+++ packages/queue/queue.go
@@ -13,13 +13,3 @@ type Details struct {
 	LastProcessed time.Time
 	LastReported  time.Time
 }
-
-// Saver interface has the save method for saving the queue information sent
-type Saver interface {
-	Save() error
-}
-
-// SaveDetails calls the QueueInfoSaver interface method to save the queue information
-func SaveDetails(s Saver) error {
-	return s.Save()
-}
