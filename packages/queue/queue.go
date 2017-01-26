b1ff344fda7c09e862076f192ed3e096260f310f
Divya Muppaneni <divya.muppaneni@safeguardproperties.com>
2017-01-26 09:38:24 -0500

    Adding the json and bson mappings

--- packages/queue/queue.go
+++ packages/queue/queue.go
@@ -6,10 +6,10 @@ import (
 
 // Details represents the information about a queue
 type Details struct {
-	Name          string
-	Type          string
-	Depth         int64
-	Rate          int64
-	LastProcessed time.Time
-	LastReported  time.Time
+	Name          string    `json:"name" bson:"name"`
+	Type          string    `json:"type" bson:"type"`
+	Depth         int64     `json:"depth" bson:"depth"`
+	Rate          int64     `json:"rate" bson:"rate"`
+	LastProcessed time.Time `json:"lastProcessed" bson:"last_processed"`
+	LastReported  time.Time `bson:"last_reported"`
 }
